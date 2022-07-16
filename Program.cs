56c6f4dad7a8376ee69c6f9219399903a43c0e22
Pedro <pedrosoaresty@gmail.com>
2022-07-16 15:39:37 -0300

    add files

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Projeto1GFT.src.Models;
+
+
+namespace projeto1gft
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Wizard wizard = new Wizard("Zezinha", 76, "Wizard");
+            Archer archer = new Archer("Robinho", 78, "Archer");
+            Beserker beserker = new Beserker("Khorno", 81, "Beserker");
+            Knight knight = new Knight("Manaus", 81, "Knight");
+            Priest priest = new Priest("Elise", 80, "Priest");
+
+        
+            Console.WriteLine(wizard.Attack());
+            Console.WriteLine(archer.Attack(8));
+            Console.WriteLine(beserker.Attack(8));
+            Console.WriteLine(knight.Attack());
+            Console.WriteLine(priest.Attack(6));
+        
+        }
+
+    }
+}
+
+
+
+
